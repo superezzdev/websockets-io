0b897f4934b054a34635c9f4a81b57770f316dba
superezz <248160976+superezz@users.noreply.github.com>
2026-08-15 15:33:54 +0530

    first commit

---
+++ sports-stream/server.js
@@ -0,0 +1,31 @@
+import { WebSocketServer, WebSocket } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", (socket, request) => {
+  const ip = request.socket.remoteAddress;
+
+  socket.on("message", (rawData) => {
+    const message = rawData.toString();
+
+    console.log({ rawData });
+
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(`Server Broadcast: ${message}`);
+      }
+    });
+  });
+
+  socket.on("error", (err) => {
+    console.error(`Error: ${err.message}: ${ip}`);
+  });
+
+  socket.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
+
+
+
+console.log("WebSocket server is running on ws://localhost:8080");
